fc4d0e7aed5f7ef04ab3da5d8003db3458f36a45
Katie Fritz <katie@katiemfritz.com>
2015-11-08 10:45:52 -0500

    SO MANY CATS IN HATS

--- app.rb
+++ app.rb
@@ -1,16 +1,14 @@
 require 'sinatra'
+require 'cat_api'
 
-class MyWebApp < Sinatra::Base
+class CatIndustriesApp < Sinatra::Base
   get '/' do
     erb :home
   end
 
-  get '/about' do
-    erb :about
+  get '/cats' do
+    @pictures = CatAPI.new.get_images(category: 'hats', results_per_page: 100)
+    erb :cats
   end
 
-  get '/madlib' do
-    erb :madlib
-  end
-  
 end
